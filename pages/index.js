edc82e84c7042808407c3e185b86b28337cbaee6
alkplima <alkplima@gmail.com>
2022-11-09 08:23:32 -0300

    Atualizando ScheduleInformation na home

--- pages/index.js
+++ pages/index.js
@@ -183,7 +183,7 @@ const Home = () => {
                         <ScheduleInformation
                             speakerPicture={speakerPicture}
                             title="palestras incríveis"
-                            overview="Hoje teremos mais um dia de palestras recheadas de informações sobre tecnologia e carreira para vocês. Não deixe de participar!"
+                            overview="Hoje teremos mais um dia de palestras recheadas de informações sobre tecnologia e carreira para você. Não deixe de participar!"
                         />
 
                     </div>
