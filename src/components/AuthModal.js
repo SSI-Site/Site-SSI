dd7ca576d34d4503f2e7194986b7858187e26664
alkplima <alkplima@gmail.com>
2023-05-02 17:49:56 -0300

    atualizando modal de login

--- src/components/AuthModal.js
+++ src/components/AuthModal.js
@@ -6,7 +6,6 @@ import useAuth from '../../hooks/useAuth';
 
 // assets
 import Logo from '../../public/images/logos/logo_principal.svg';
-import GoogleLogo from '../../public/images/login_icons/google_logo_white.png';
 
 const AuthModal = ({ onClose }) => {
 
@@ -55,11 +54,14 @@ const AuthModal = ({ onClose }) => {
                             <img className='logo' src={Logo} alt="Logo SSI 2023" />
                         </figure>
 
-                        <h4>Entrar com</h4>
+                        <h6>Entrar com</h6>
                         <div></div>
 
                         <SigninBtn onClick={handleGoogleSignIn}>
-                            <img src={GoogleLogo} alt="Logo da Google branco" />
+                            {/* Logo da Google */}
+                            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1.064 5.51C1.89601 3.85324 3.17237 2.46051 4.75043 1.48747C6.32849 0.514427 8.14606 -0.000583569 10 4.96231e-07C12.695 4.96231e-07 14.959 0.991001 16.69 2.605L13.823 5.473C12.786 4.482 11.468 3.977 10 3.977C7.395 3.977 5.19 5.737 4.405 8.1C4.205 8.7 4.091 9.34 4.091 10C4.091 10.66 4.205 11.3 4.405 11.9C5.191 14.264 7.395 16.023 10 16.023C11.345 16.023 12.49 15.668 13.386 15.068C13.9054 14.726 14.3501 14.2822 14.6932 13.7635C15.0363 13.2448 15.2706 12.6619 15.382 12.05H10V8.182H19.418C19.536 8.836 19.6 9.518 19.6 10.227C19.6 13.273 18.51 15.837 16.618 17.577C14.964 19.105 12.7 20 10 20C8.68663 20.0005 7.38604 19.7422 6.17254 19.2399C4.95905 18.7375 3.85645 18.0009 2.92776 17.0722C1.99907 16.1436 1.2625 15.041 0.760135 13.8275C0.257774 12.614 -0.000524861 11.3134 8.00714e-07 10C8.00714e-07 8.386 0.386001 6.86 1.064 5.51Z" fill="#F3F3F3"/>
+                            </svg>
                             <span>Google</span>
                         </SigninBtn>
                     </ModalBody>
@@ -173,8 +175,8 @@ const ModalBody = styled.div`
         width: 100%;
     }
 
-    h4 {
-        margin-bottom: 4px;
+    h6 {
+        margin-bottom: 1rem;
     }
 
     div {
@@ -190,30 +192,56 @@ const SigninBtn = styled.button`
     display: flex;
     align-items: center;
     justify-content: center;
-    width: 65%;
-    padding: 16px;
+    width: 9.1875rem;
+    height: 3rem;
+    padding: 0.5625rem 1.3125rem;
     margin-bottom: 5px;
-    border-radius: 12px;
-    border: none;
-    background-color: var(--color-primary);
-    transition: 0.2s;
+    border-radius: 9px;
+    border: 3px solid var(--color-neutral-50);
+    background-color: transparent;
+    transition: 500ms;
     cursor: pointer;
 
     span {
-        font-family: 'Space_Mono_Bold';
-        font-weight: 400;
+        font: 400 1rem/1.25rem 'Space_Mono_Bold';
     }
 
-    img {
+    svg {
         width: 20px;
         margin-right: 10%;
     }
 
     &:hover {
-        background-color: var(--color-primary-900);
+        background-color: var(--color-neutral-50);
+        
+        span {
+            color: var(--color-neutral-900);
+        }
+
+        svg path {
+            fill: var(--color-neutral-900);
+        }
     }
 
     &:active {
-        background-color: var(--color-primary-800);
+        background-color: var(--color-neutral-100);
+        border-color: var(--color-neutral-100);
+
+        span {
+            color: var(--color-neutral-900);
+        }
+
+        svg path {
+            fill: var(--color-neutral-900);
+        }
+    }
+        
+    @media (min-width:560px) {
+        width: 9.6875rem;
+        height: 3rem;
+
+        span {
+            font: 400 1.125rem/1.5rem 'Space_Mono_Bold';
+        }
     }
 `
