c43dcd042918dcb7545af013530529bf7f48a9a5
alkplima <alkplima@gmail.com>
2023-08-18 20:46:32 -0300

    Tirando console.log do usuário

--- pages/user.js
+++ pages/user.js
@@ -37,7 +37,7 @@ const User = () => {
 
     const checkUserRegister = () => {
         if (!user) return;
-        console.log("Usuário:", user);
+        // console.log("Usuário:", user);
 
         setIsLoading(true);
 
