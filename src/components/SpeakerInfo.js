85bbfc9f0ce9b586262daadffcd1b885e12ef2b2
Caio Bernardo <caio.2004vb@gmail.com>
2024-08-20 17:56:39 -0300

    feat(SpeakerInfo.js): animation for desktop
    - Animation for desktop
    - More responsiveness

--- src/components/SpeakerInfo.js
+++ src/components/SpeakerInfo.js
@@ -16,15 +16,14 @@ const SpeakerInfo = ({ speaker }) => {
 					<div className='speaker-info'>
 						<h6>{speaker['name']}</h6> 
 						{/* TODO: definir `cargo`, já que o atributo não existe em `speaker` */}
-						<p>Cargo</p>
-					</div>
-					<div>
 						<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 							<path d="M17.9999 7.05C17.9999 6.78478 17.8946 6.53043 17.707 6.34289C17.5195 6.15536 17.2652 6.05 16.9999 6.05L8.99994 6C8.73472 6 8.48037 6.10536 8.29283 6.29289C8.1053 6.48043 7.99994 6.73478 7.99994 7C7.99994 7.26522 8.1053 7.51957 8.29283 7.70711C8.48037 7.89464 8.73472 8 8.99994 8H14.5599L6.28994 16.29C6.19621 16.383 6.12182 16.4936 6.07105 16.6154C6.02028 16.7373 5.99414 16.868 5.99414 17C5.99414 17.132 6.02028 17.2627 6.07105 17.3846C6.12182 17.5064 6.19621 17.617 6.28994 17.71C6.3829 17.8037 6.4935 17.8781 6.61536 17.9289C6.73722 17.9797 6.86793 18.0058 6.99994 18.0058C7.13195 18.0058 7.26266 17.9797 7.38452 17.9289C7.50638 17.8781 7.61698 17.8037 7.70994 17.71L15.9999 9.42V15C15.9999 15.2652 16.1053 15.5196 16.2928 15.7071C16.4804 15.8946 16.7347 16 16.9999 16C17.2652 16 17.5195 15.8946 17.707 15.7071C17.8946 15.5196 17.9999 15.2652 17.9999 15V7.05Z" fill="#F3F3F3"/>
 						</svg>
-
 					</div>
-					{/* INFO: fora do escopo da task, pode ser reutilizado em tasks futuras*/}
+					<div className='speaker-info-cargo'>
+						<p>Cargo</p>
+					</div>
+					{/* INFO: website icon, maybe used later*/}
 
 					{/* <div> */}
 					{/* 	{speaker['website'] && */}
@@ -57,101 +56,78 @@ const SpeakerContainer = styled.div`
 		flex: 40%;
 	}
 
-	.speaker-info-container {
-		flex: 60%;
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-	}
-
     .speaker-picture {
         width: 100%;
         min-width: 5rem;
         max-width: 34rem;
         min-height: 8rem;
         max-height: 14rem;
-        /* border-radius: 16px; */
         object-fit: cover;
     }
 
+	.speaker-info-container {
+		flex: 60%;
+	}
+
 	.speaker-info {
+		display: flex;
+		justify-content: space-between;
+
 		h6 {
         	font: 700 1rem/1.5rem 'AT Aero Bold';
 		}
 
+		svg {
+			transform: translateY(0.5em);
+		}
+	}
+	
+	.speaker-info-cargo {
 		p {
-        	font: 400 0.75rem/1.25rem 'AT Aero';
+			font: 700 0.75rem/1.25rem 'AT Aero';
 		}
 	}
 
+	@media(min-width:800px) {
+		flex-direction: column;
 
-    /* .speaker-image-name { */
-    /*     display: flex; */
-    /*     flex-direction: column; */
-    /*     justify-content: center; */
-    /*     align-items: center; */
-    /*     gap: 0.5rem; */
-    /*     width: 100%; */
-    /* } */
+		.speaker-info {
+			background: linear-gradient(to right, var(--color-neutral-50) 50%, transparent 50%);
+			background-position: right;
+			background-size: 202% 100%;
+			transition: 250ms all ease-out;
+			svg {
+				transform: translateY(0em);
+			}
 
-	    
-	/*INFO: no use to speaker-link*/
+			&:hover {
+				background-position: left;
+				h6 {
+					color: #161616;
+				}
+				svg path {
+					fill: #161616;
+					transition: fill 300ms ease;
+				}
+			}
+		}
 
-    /* .speaker-links { */
-    /*     display: flex; */
-    /*     flex-direction: row; */
-    /*     align-items: center; */
-    /*     justify-content: space-around; */
-    /*     gap: 0.5rem; */
-    /**/
-    /*     a { */
-    /*         display: flex; */
-    /*         align-items: center; */
-    /*         justify-content: center; */
-    /*         padding: 0.5rem; */
-    /*         width: 2rem; */
-    /*         height: 2rem; */
-    /*         background-color: var(--color-neutral-700); */
-    /*         border-radius: 8px; */
-    /*         transition: 0.3s all ease-in-out; */
-    /**/
-    /*         &:hover { */
-    /*             background-color: var(--color-neutral-600); */
-    /*         } */
-    /*     } */
-    /* } */
-
-    @media (min-width:400px) {
-        .speaker-name {
-            font: 400 0.875rem/1.25rem 'AT Aero';
-        }
-
-        .speaker-links {
-            a {
-                padding: 0;
-                width: 2.75rem;
-                height: 2.75rem;
-            }
-        }
-    }
+		.speaker-info-container {
+			width: 100%;
+			max-width: 20rem;
+			display: flex;
+			flex-direction: column;
+		}
+	}
 
     @media (min-width:1024px) {
         gap: 1rem;
 
-        /* .speaker-image-name { */
-        /*     gap: 1rem; */
-        /* } */
-
-        .speaker-name {
-            font: 400 1.25rem/1.5rem 'AT Aero Bold';
-        }
-
         .speaker-picture {
             height: 23.5rem;
             max-height: none;
-            border-radius: 16px;
             object-fit: cover;
+			width: 20rem;
         }
-
     }
 `
