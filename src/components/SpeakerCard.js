fbc79f6931a18bac40d73fc40d8e7fb122a6e5c9
F1NH4WK <eduardo.ar33@gmail.com>
2024-08-25 22:02:36 -0300

    feat: Desktop & Mobile support

--- src/components/SpeakerCard.js
+++ src/components/SpeakerCard.js
@@ -17,7 +17,7 @@ const SpeakerCard = () => {
         <SpeakerWrapper>
             <SpeakerContent>
                 <SpeakerHead>
-                    <h4>Palestrante</h4>    
+                    <h6>Palestrante</h6>    
                     <img src = {Close}/>
                 </SpeakerHead>
 
@@ -33,33 +33,38 @@ const SpeakerCard = () => {
                         </div>
 
                         <div>
-                            <h6>Cargo</h6>
-                            <p>Estagiário de dados | Microsoft</p>
+                            <label>Cargo</label>
+                            <p>Website Developer</p>
                         </div>
                     </div>
                 </SpeakerInfo>
 
-                <SpeakerDesc>
-                    <h6>Sobre</h6>
+                <SpeakerDesc>  
                     <div>
-                        <p>Lorem ipsum dolor sit amet consectetur. Viverra consequat pharetra mauris diam integer purus morbi nibh. Nec odio sodales gravida at vitae. Lacus eleifend amet purus scelerisque felis. Lorem sodales commodo enim et id. Tincidunt tempor viverra consectetur netus feugiat cras volutpat ipsum. Eget morbi egestas semper diam adipiscing ac amet ut. Ut sagittis aliquet pharetra ut bibendum quisque rhoncus mattis. Lectus sed gravida duis purus integer quis. Vulputate vestibulum ut non vitae mi quis. Lorem ipsum dolor sit amet consectetur. Viverra consequat pharetra mauris diam integer purus morbi nibh. Nec odio sodales gravida at vitae. Lacus eleifend amet purus scelerisque felis. Lorem sodales commodo enim et id. Tincidunt tempor viverra consectetur netus feugiat cras volutpat ipsum. Eget morbi egestas semper diam adipiscing ac amet ut. Ut sagittis aliquet pharetra ut bibendum quisque rhoncus mattis. Lectus sed gravida duis purus integer quis. Vulputate vestibulum ut non vitae mi quis.</p>
+                        <h6>Sobre</h6>
+                    </div>
+                    
+                    <div>
+                        <p>Lorem ipsum dolor sit amet consectetur. Viverra consequat pharetra mauris diam integer purus morbi nibh. Nec odio sodales gravida at vitae. Lacus eleifend amet purus scelerisque felis. Lorem sodales commodo enim et id. Tincidunt tempor viverra consectetur netus feugiat cras volutpat ipsum. Eget morbi egestas semper diam adipiscing ac amet ut. Ut sagittis aliquet pharetra ut bibendum quisque rhoncus mattis. Lectus sed gravida duis purus integer quis. Vulputate vestibulum ut non vitae mi quis. </p>
                     </div>
                 </SpeakerDesc>
 
                 <SocialMedia>
                     <a href = "" target="_blank" aria-label = "Linkedin da Semana de Sistemas de Informação">
-                        <img src = {LinkedinIcon} alt = "Linkedin"/>
+                        <img src = { LinkedinIcon } alt = "Linkedin"/>
                     </a>
 
                     <a href = "" target = "_blank" aria-label = "Instagram da Semana de Sistemas de Informação">
-                        <img src = {InstagramIcon} alt = "Instagram"/>                        
+                        <img src = { InstagramIcon } alt = "Instagram"/>                        
                     </a>
-
+                    
                     {/* SLOTS PARA MAIS*/}
                 </SocialMedia>
             </SpeakerContent>
 
-            <img src = {SpeakerBottomDesktop} className = 'bottomImg'/>
+            <div className = 'bottomImg'>
+                <img src = {SpeakerBottomDesktop} />
+            </div>
         </SpeakerWrapper>
     )
 }
@@ -79,6 +84,14 @@ const SpeakerWrapper = styled.div`
 
     .bottomImg{
         width: 100%;
+        height: 10%;
+
+        img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            object-position: left;
+        }
     }
 
     @media screen and (min-width: 1024px){
@@ -91,23 +104,17 @@ const SpeakerContent = styled.div`
     width: 100%;
     height: 100%;
     position: relative;
-    padding: 1.7em;
     display: flex;
     flex-direction: column;
-    gap: 1em;   
-
-    @media screen and (min-width: 1024px){
-        padding: 0em;
-        gap: .5em;
-    }
+    gap: .5em; 
 `
 
 const SpeakerHead = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
-    padding: 1em 0em;
-
+    padding: 1.75em;
+    
     @media screen and (min-width: 1024px){
         display: none;
     }
@@ -117,32 +124,47 @@ const SpeakerInfo = styled.div`
     width: 100%;
     display: flex;
     align-items: center;
-    justify-content: space-between;
-    gap: 1rem;
+    gap: 1.75em;
+    padding: 0em 1.75em;
+    margin-bottom: 1.7em; // 1.7em + gap .5em
+
+    label{
+        font-family: 'AT Aero Bold';
+    }
+
+    .headTextWrapper{
+        display: flex;
+        flex-direction: column;
+        gap: .5em;
+    }
 
     .imgDiv{
         border: 1px solid blue;
-        width: 25%;
-    }
+        width: 40%;
+        height: 100%;
 
-    div > img{
-        width: 100%;
+        img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
     }
 
     @media screen and (min-width: 1024px){
         justify-content: flex-start;
-        gap: 4rem;
+        gap: 4em;
         background-color: var(--color-primary-900);
+        padding: 0em;
+
+        .imgDiv{
+            max-width: 30%;
+        }
 
         .headTextWrapper{
-            display: flex;
-            flex-direction: column;
             gap: 1rem;
 
-            & > div:nth-child(2){
-                h6 {
-                    font-size: 1.2rem;
-                }
+            label {
+                font-size: 1.2rem;
             }
         }
     }
@@ -152,28 +174,28 @@ const SpeakerDesc = styled.div`
     display: flex;
     flex-direction: column;
     gap: .5rem;
+    width: 100%;
     height: 100%;
-    overflow: hidden;
-    padding: 4rem;
-    max-height: 21em;
-
-    div {
-        width: 90%;
-    }
+    padding: 0em 1.75em;
+    max-height: 23.5em;
 
     p {
+        width: 100%;
         height: 100%;
         overflow: scroll;
     }
     
     @media screen and (min-width: 1024px){
         flex-direction: row;
-        gap: 1rem;
-        height: 100%;
-
-        h6{
-            width: 35%;
+        justify-content: space-between;
+        
+        div:nth-child(2){
+            width: 72%; // imgWidth = 30%, então 70% + algoAí deixa alinhado.
         }
+    }
+
+    @media screen and (min-width: 1440px){
+        padding: 4em;
     }
 `
 
@@ -181,13 +203,9 @@ const SocialMedia = styled.div`
     width: 100%;
     display: flex;
     gap: 1rem;
-    padding: 0rem 4rem;
+    padding: 0rem 1.75em;
     position: absolute;
-    bottom: 5%;
-
-    a {
-        border: 1px solid red;
-    }
+    bottom: 3%;
 
     img {
         width: 100%;
