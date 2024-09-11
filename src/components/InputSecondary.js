b80cf0eb801c8850a567b361a6f071237c782a47
Miguel <75583778+Miguel03706@users.noreply.github.com>
2024-09-11 12:31:19 -0300

    Update InputSecondary.js

--- src/components/InputSecondary.js
+++ src/components/InputSecondary.js
@@ -15,6 +15,19 @@ const InputSecondary = styled.input`
     transition: all 0.3s ease-in-out;
     white-space: nowrap;
     text-align: center;
+    font-family: 'AT Aero Bold';
+
+    /* Remove setas do input number em navegadores */
+    &[type="number"]::-webkit-outer-spin-button,
+    &[type="number"]::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+
+    /* Remove setas do input number no Firefox */
+    &[type="number"] {
+        -moz-appearance: textfield;
+    }
 
     &:focus-visible {
         border: 2px solid var(--color-primary) !important;
