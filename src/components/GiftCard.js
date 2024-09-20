c54576259d64eb97d24219ee9a10da29833d9649
Caio Bernardo <caio.2004vb@gmail.com>
2024-09-19 23:33:26 -0300

    update(GiftCard.js): complete revampe based on UserGiftCard

--- src/components/GiftCard.js
+++ src/components/GiftCard.js
@@ -1,162 +1,184 @@
-import { useState } from 'react';
 import styled from 'styled-components';
 
-// components
-import GiftModal from './GiftModal';
+import giftBox from '../../public/images/gifts/gift-box.png';
 
-const GiftCard = ({ image, name, totalPres, presentialPres }) => {
+const GiftCard = ({ index, image, name, totalPres, presentialPres }) => {
+	return (
+		<GiftContainer tabIndex={0}>
+			<div className={"gift-card-front"} >
+				<figure>
+					<img className="gift-img" src={image} alt={`Brinde ${name} SSI`} />
+				</figure>
+				<h6>{name}</h6>
+			</div>
 
-    const [btnState, setBtnState] = useState(false);
-  
-    const handleBtnClick = () => {
-        setBtnState(!btnState);
-    };
+			<div className='gift-card-back' id={"gift" + index}>
+				<img className="icon-image" src={giftBox} alt="gift box" />
 
-    return (
-        <>
-            <GiftWrapper>
-                <div className='gift-box' onClick={() => handleBtnClick()}>
-                    <div className='gift-image'>
-                        <img src={image} alt={`Logo ${name}`}></img>
-                    </div>
-                    <p>{name}</p>
-                </div>
-            </GiftWrapper>
-            <GiftPopUpBackdrop id="backdrop-parent" className={btnState ? "show-modal" : ""}>
-                <GiftPopUp id="popup">
-                    <GiftModal
-                        onClose={() => handleBtnClick()}
-                        image={image}
-                        name={name}
-                        totalPres={totalPres}
-                        presentialPres={presentialPres}
-                        />
-                </GiftPopUp>
-            </GiftPopUpBackdrop>
-        </>
-    )
+				<p className='card-back-text'>Para resgatar esse brinde você deve participar de um total de <span>{totalPres}</span> atividades,
+					sendo <span>{presentialPres}</span> delas presencialmente.</p>
+				
+			</div>
+			<button id={"btn" + index} className='info-button' onClick={() => flip(index)} tabIndex={0}>
+				<svg width="12" height="18" viewBox="0 0 12 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path d="M11.6567 5.96199L10.2388 7.37299L6.98375 4.10299L6.97075 17.708L4.97075 17.706L4.98375 4.13799L1.75375 7.35299L0.34375 5.93599L6.01375 0.291992L11.6567 5.96199Z" fill="#161616" />
+				</svg>
+			</button>
+		</GiftContainer>
+	)
+
+}
+
+const flip = (index) => {
+	let card = document.getElementById("gift" + index)
+	let button = document.getElementById("btn" + index)
+	card.classList.toggle("info-show")
+	button.classList.toggle("button-flip")
 }
 
 export default GiftCard;
 
-const GiftPopUpBackdrop = styled.div`
-    position: fixed;
-    height: 100%;
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background-color: rgba(0, 0, 0, 0.5);
-    left: 0;
-    top: 0;
-    display : none;
-    z-index: 99;
-
-    &.show-modal {
-        display: flex;
-    }
-`
-  
-const GiftPopUp = styled.div`
-    display : flex;
-    align-items: center;
-    justify-content: center;
-`
-
-const GiftWrapper = styled.div`
+const GiftContainer = styled.div`
+	width: 100%;
+    max-width: 26.3rem;
+	height: 24.625rem;
+	display: flex;
+	gap: 1rem;
+	align-items: center;
+	justify-content: center;
+	overflow-y: hidden;
     position: relative;
-    background-color: var(--color-neutral-800);
-    border: 4px solid var(--color-primary-700);
-    border-radius: 8px;
-    transition: 0.3s;
 
-    &:active {
-        background-color: var(--color-neutral-600);
-    }
+	.gift-card-front {
+		width: 100%;
+        max-width: 26.3rem;
+		height: 24.625rem;
+		display: flex;
+		gap: 2rem;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		position: absolute;
+        left: 0;
+        right: 0;
+		background-color: var(--color-neutral-800);
+		border: 0.0625rem solid #7f7f7f;
 
-    .gift-box {
-        cursor: pointer;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        width: 9.25rem;
-        height: 9rem;
-        padding: 0.65625rem;
-        gap: 0.3125rem;
-
-        .gift-image {
+        figure {
+            height: 12rem;
             display: flex;
             align-items: center;
-            height: 5.55rem;
-            border-radius: 8px;
-
-            img {
-                width: 100%;
-                height: 100%;
-                object-fit: contain;
-            }
+            justify-content: center;
         }
 
-        p {
-            font-size: 0.85rem; // fonte menor para evitar quebra de linha
-            font-family: 'AT Aero Bold';
-            font-weight: 400;
-            text-align: center;
+		.gift-img {
+            max-width: 100%;
+            max-height: 100%;
+            height: auto;
+            width: auto;
+		}
+	}
+
+	.gift-card-back {
+		width: 100%;
+        max-width: 26.3rem;
+		height: 24.625rem;
+		transition: 0.1s;
+		translate: 0 101%;
+		position: relative;
+		padding: 1.5rem;
+		display: flex;
+		flex-direction: column;
+		gap: 0.5rem;
+		align-items: center;
+		justify-content: center;
+		background-color: var(--color-neutral-50);
+		border: 0.0625rem solid #7f7f7f;
+
+		.icon-image {
+			width: 5rem;
+			height: 5rem;
+            margin-bottom: 0.5rem;
         }
+
+		.card-back-text {
+			text-align: center;
+			color: var(--color-neutral);
+			font: 700 1.125rem/1.5rem 'AT Aero Bold';
+
+			span {
+				font: 700 1.125rem/1.5rem 'AT Aero Bold';
+				color: var(--color-primary);
+			}
+		}
+		
+	}
+
+	.info-button {
+        border: 0;
+        display: flex;
+        position: absolute;
+        width: 3rem;
+        height: 3rem;
+        background-color: var(--color-neutral-50);
+        right: 1rem;
+        bottom: 1rem;
+        align-items: center;
+        justify-content: center;
+
+		svg {
+			transition: 0.3s;
+		}
     }
 
-    @media (min-width:428px) {
+	.info-show {
+		translate: 0 0;
+	}
 
-        .gift-box {
-            width: 11.375rem;
-            height: 10rem;
-            p {
-                font-size: 1rem;
+	.button-flip {
+        svg {
+            transform: rotate(-180deg);
+
+            path {
+                fill: white;
             }
         }
+        background-color: var(--color-primary);
     }
 
-    @media (min-width:1000px) {
-        width: 18rem;
-        height: 14.75rem;
-        border: 4px solid transparent;
-
-        &:hover:not(:active) {
-            border: 4px solid var(--color-primary-700);
-            background-color: var(--color-neutral-700);  
-            animation: shake 1s;
-            animation-iteration-count: infinite; 
-        }
-
-        @keyframes shake {
-            0% { transform: translate(1px, 1px) rotate(0deg); }
-            10% { transform: translate(-1px, -2px) rotate(-1deg); }
-            20% { transform: translate(-3px, 0px) rotate(1deg); }
-            30% { transform: translate(3px, 2px) rotate(0deg); }
-            40% { transform: translate(1px, -1px) rotate(1deg); }
-            50% { transform: translate(-1px, 2px) rotate(-1deg); }
-            60% { transform: translate(-3px, 1px) rotate(0deg); }
-            70% { transform: translate(3px, 1px) rotate(-1deg); }
-            80% { transform: translate(-1px, -1px) rotate(1deg); }
-            90% { transform: translate(1px, 2px) rotate(0deg); }
-            100% { transform: translate(1px, -2px) rotate(-1deg); }
-        }
-        
-        .gift-box {
-            width: 100%;
-            height: 100%;
-            padding: 2rem 3rem;
-            padding: 1.5rem;
-
-            .gift-image {
-                height: 9rem;
-            }
-
-            p {
-                font: 400 1.15rem/1.5rem 'AT Aero Bold'; // menor que no Figma, paa evitar quebra de linha
-                font: 400 1.5rem/1.75rem 'AT Aero Bold';
-            }
-        }
+    button:focus-visible {
+        outline: 2px solid var(--color-primary);
+        outline-offset: 2px;
     }
+
+	@media (min-width:1021px) {
+        width: 26.3rem;
+
+        .gift-card-front {
+            width: 26.3rem;
+            height: 24.625rem;
+            gap: 1.12rem;
+        }
+
+        .gift-card-back {
+            width: 26.3rem;
+            height: 24.625rem;
+            padding: 2rem;
+        }
+
+		&:hover, &:focus-visible {
+			.gift-card-back {
+				translate: 0 0;
+			}
+		}
+
+        &:focus-visible {
+            outline: 2px solid var(--color-primary);
+            outline-offset: 2px;
+        }
+
+		.info-button {
+			display: none;
+		}
+	}
 `
