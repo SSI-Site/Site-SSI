0de9653202254f1fb4f9698f2b275820ee1f970e
alkplima <alkplima@gmail.com>
2024-09-21 16:05:36 -0300

    fix: removendo animação para nomes que não têm link

--- src/components/MemberCard.js
+++ src/components/MemberCard.js
@@ -52,11 +52,11 @@ const MemberCard = ({ name, image, departments, linkedin, colorScheme, phrase })
                 </figure>
             </div>
             <div className={'card-back b' + (colorScheme%5)} id={'back b' + (colorScheme)}>
-                <div className='member-name'>
+                <div className={`member-name ${linkedin ? 'animate' : ''}`}>
                     {linkedin ?
                         <>
                             <a href={linkedin} target="_blank" rel="noreferrer">
-                                <h6 className='name-linkedin'>{name}</h6>
+                                <h6>{name}</h6>
                                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M17.9999 7.05C17.9999 6.78478 17.8946 6.53043 17.707 6.34289C17.5195 6.15536 17.2652 6.05 16.9999 6.05L8.99994 6C8.73472 6 8.48037 6.10536 8.29283 6.29289C8.1053 6.48043 7.99994 6.73478 7.99994 7C7.99994 7.26522 8.1053 7.51957 8.29283 7.70711C8.48037 7.89464 8.73472 8 8.99994 8H14.5599L6.28994 16.29C6.19621 16.383 6.12182 16.4936 6.07105 16.6154C6.02028 16.7373 5.99414 16.868 5.99414 17C5.99414 17.132 6.02028 17.2627 6.07105 17.3846C6.12182 17.5064 6.19621 17.617 6.28994 17.71C6.3829 17.8037 6.4935 17.8781 6.61536 17.9289C6.73722 17.9797 6.86793 18.0058 6.99994 18.0058C7.13195 18.0058 7.26266 17.9797 7.38452 17.9289C7.50638 17.8781 7.61698 17.8037 7.70994 17.71L15.9999 9.42V15C15.9999 15.2652 16.1053 15.5196 16.2928 15.7071C16.4804 15.8946 16.7347 16 16.9999 16C17.2652 16 17.5195 15.8946 17.707 15.7071C17.8946 15.5196 17.9999 15.2652 17.9999 15V7.05Z" fill="#F3F3F3"/>
                                 </svg>
@@ -168,6 +168,15 @@ const MemberWrapper = styled.div`
             background-size: 200%;
             background-position-x: 200%;
             background-repeat: no-repeat;
+
+            h6 {
+                font: 700 1.5rem/1.75rem 'AT Aero Bold';
+                transition: 0.2s;
+                color: ${colorSchemes[1].textColor}
+            }
+        }
+
+        .animate {
             transition: background-position 0.2s;
 
             a {
@@ -184,12 +193,6 @@ const MemberWrapper = styled.div`
                 svg {
                     width: 2.5rem;
                 }
-            }
-
-            h6 {
-                font: 700 1.5rem/1.75rem 'AT Aero Bold';
-                transition: 0.2s;
-                color: ${colorSchemes[1].textColor}
             }
 
             &:hover, &:focus-visible {
@@ -252,7 +255,9 @@ const MemberWrapper = styled.div`
             h6 {
                 color: ${colorSchemes[0].textColor};
             }
+        }
 
+        .animate {
             a {
                 svg path {
                     fill: ${colorSchemes[0].textColor};
@@ -295,7 +300,9 @@ const MemberWrapper = styled.div`
             h6 {
                 color: ${colorSchemes[1].textColor};
             }
+        }
 
+        .animate {
             a {
                 svg path {
                     fill: ${colorSchemes[1].textColor};
@@ -338,7 +345,9 @@ const MemberWrapper = styled.div`
             h6 {
                 color: ${colorSchemes[2].textColor};
             }
+        }
 
+        .animate {
             a {
                 svg path {
                     fill: ${colorSchemes[2].textColor};
@@ -382,7 +391,9 @@ const MemberWrapper = styled.div`
             h6 {
                 color: ${colorSchemes[3].textColor};
             }
+        }
 
+        .animate {
             a {
                 svg path {
                     fill: ${colorSchemes[3].textColor};
@@ -425,7 +436,9 @@ const MemberWrapper = styled.div`
             h6 {
                 color: ${colorSchemes[4].textColor};
             }
+        }
 
+        .animate {
             a {
                 svg path {
                     fill: ${colorSchemes[4].textColor};
