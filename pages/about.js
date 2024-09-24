61a40dca1e07f2f9c9b34ec5f6e4e4b75ac26475
alkplima <alkplima@gmail.com>
2024-09-24 00:00:21 -0300

    fix: corrigindo padding novamente

--- pages/about.js
+++ pages/about.js
@@ -701,7 +701,7 @@ const LastYearSection = styled.section`
 
             .lastyear-text {
                 border: none;
-                padding: 4.5rem auto 0 0;
+                padding: 4.5rem 1rem 0 0;
                 gap: 1.5rem;
                 max-width: 25.6rem;
 
