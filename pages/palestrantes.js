2f05af56bd36f2c8a2a0ee69b422ba8b30c6be5c
F1NH4WK <eduardo.ar334@gmail.com>
2025-05-25 01:13:37 -0300

    feat: add palestrante pages

---
+++ pages/palestrantes.js
@@ -0,0 +1,13 @@
+import Meta from '../src/infra/Meta';
+
+const Palestrantes = () => {
+    return (
+        <>
+          <Meta title='SSI 2025 | Palestrantes' />
+            <h1>Palestrantes</h1>
+        </>
+    )
+    
+}
+
+export default Palestrantes
