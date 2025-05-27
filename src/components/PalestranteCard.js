90794d0092a2e1c9ff462061f358201863a36e3d
F1NH4WK <eduardo.ar334@gmail.com>
2025-05-27 15:19:17 -0300

    fix: general issues

--- src/components/PalestranteCard.js
+++ src/components/PalestranteCard.js
@@ -150,73 +150,12 @@ const PalestranteSocialMediaItem = ({ icon, link, alt }) => {
     )
 }
 
-// Componente para exibir informações de uma palestra
-const Palestra = ({ dataPalestra, tituloPalestra, metadata }) => {
-    return (
-        <PalestraContainer>
-            <PalestraData>
-                {dataPalestra}
-            </PalestraData>
-
-            <PalestraTitulo>
-                {tituloPalestra}
-            </PalestraTitulo>
-
-            <PalestraTags>
-                {
-                    metadata.presencial && (
-                        <PalestraTag>
-                            Presencial
-                        </PalestraTag>
-                    )
-                }
-            </PalestraTags>
-        </PalestraContainer>
-    )
-}
+export default PalestranteCard;
 
 const PalestranteArrowUp = styled.img`
     width: 3rem;
     height: 3rem;
     cursor: pointer;
-`;
-
-const PalestraTags = styled.div`
-    display: flex;
-    gap: 0.8rem;
-    flex-wrap: wrap;
-`;
-
-const PalestraTag = styled.span`
-    font-weight: 400;
-    color: var(--color-neutral-50);
-    background: var(--background-badge-brand-purple-500, #9638FF);
-    padding: 0.25rem 0.5rem;
-    // ALREADY EXISTS
-`;
-
-const PalestrantePalestras = styled.div`
-    display: flex;
-    flex-direction: column;
-`;
-
-export default PalestranteCard;
-
-const PalestraContainer = styled.div`
-    padding: 1.5rem;
-    display: flex;
-    flex-direction: column;
-    gap: 1.5rem;
-    border: 1px solid var(--outline-neutrals-primary, #808080);
-`;
-
-const PalestraData = styled.p`
-    font-weight: 500;
-`;
-
-const PalestraTitulo = styled.h5`
-    font-weight: 500;
-    margin-top: 0;
 `;
 
 const PalestranteContainer = styled.div`
