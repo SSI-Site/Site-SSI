d2e4f4cfce2c8f92ac752a461b9fc32a0fb2806c
F1NH4WK <eduardo.ar334@gmail.com>
2025-07-18 16:36:23 -0300

    fix: scrollbar-width

--- pages/co.js
+++ pages/co.js
@@ -216,7 +216,7 @@ const COFilterContainer = styled.div `
         scroll-behavior: smooth;
         scroll-snap-align: center;
         scroll-snap-type: x mandatory;
-        scrollbar-width: hidden;  // hidden
+        scrollbar-width: noneq;  // hidden
     }
 
     @media (min-width:1021px) {
