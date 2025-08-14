a5a9f375925078ad5a003da4b8c89a7329f07f2e
F1NH4WK <eduardo.ar334@gmail.com>
2025-08-14 20:11:30 -0300

    fix: ssitalk

--- data/sponsors.js
+++ data/sponsors.js
@@ -6,7 +6,6 @@ import SSITalks from '../public/images/partners/badges/logo-badge-parceria.png'
 import PetSI from '../public/images/partners/badges/logo-badge-pet.png'
 import Hype from '../public/images/partners/badges/logo-badge-hype.png'
 import idwall from '../public/images/partners/badges/logo-badge-idwall.png'
-import SSITalks from '../public/images/partners/badges/logo-badge-parceria.png'
 import neologica from '../public/images/partners/badges/logo-badge-neologica.png'
 
 const sponsorImages  = {
