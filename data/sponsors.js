8da7db4dcbaa27df027acda5c64c2aaa940a8cb4
F1NH4WK <eduardo.ar334@gmail.com>
2025-08-14 18:35:28 -0300

    fix: new sponsors

--- data/sponsors.js
+++ data/sponsors.js
@@ -3,9 +3,11 @@ import Alura from '../public/images/partners/badges/logo-badge-alura.png'
 import Bravium from '../public/images/partners/badges/logo-badge-bravium.png'
 import RVentures from '../public/images/partners/badges/logo-badge-r2-ventures.png'
 import SSITalks from '../public/images/partners/badges/logo-badge-parceria.png'
-import PetSI from '../public/images/partners/badges/logo-badge-pet.svg'
+import PetSI from '../public/images/partners/badges/logo-badge-pet.png'
 import Hype from '../public/images/partners/badges/logo-badge-hype.png'
 import idwall from '../public/images/partners/badges/logo-badge-idwall.png'
+import SSITalks from '../public/images/partners/badges/logo-badge-parceria.png'
+import neologica from '../public/images/partners/badges/logo-badge-neologica.png'
 
 const sponsorImages  = {
     'totvs': TOTVS,
@@ -15,7 +17,9 @@ const sponsorImages  = {
     'hype': Hype,
     'ssi talks': SSITalks,
     'pet si':PetSI,
-    'idwall': idwall
+    'idwall': idwall,
+    'ssi talks': SSITalks,
+    'neologica':neologica,
 }
 
 export default sponsorImages
