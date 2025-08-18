bc244f6948afd0e06d33b44730e79b00f97bf94a
F1NH4WK <eduardo.ar334@gmail.com>
2025-08-18 19:38:56 -0300

    fix: day

--- src/components/DateStamp.js
+++ src/components/DateStamp.js
@@ -11,12 +11,13 @@ const DateStamp = ({ day, isActive }) => {
     const currentDay = current.getDate();
     const month = current.getMonth() + 1;
     const year = current.getFullYear();
+    console.log(numericDay)
 
     return (
         <DateWrapper $isActive={isActive} >
             <div className='day-emoji'>
                 <h5 className='day'>Dia {relativeDay}</h5>
-                {(new Date(`${year}-${month}-${currentDay}`) > new Date(`2024-10-${numericDay}`)) &&
+                {(new Date(`${year}-${month}-${currentDay}`) > new Date(`2025-09-${numericDay}`)) &&
                     // CheckIcon 
                     <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40" fill="none">
                         <path fill-rule="evenodd" clip-rule="evenodd" d="M33.3333 5H6.66667H5V6.66667V33.3333V35H6.66667H33.3333H35V33.3333V6.66667V5H33.3333ZM11.2667 19.595L18.3383 26.6667L30.1233 14.8817L27.7667 12.525L18.3383 21.9533L13.6233 17.2383L11.2667 19.595Z" fill="white"/>
