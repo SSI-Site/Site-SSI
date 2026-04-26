af87842b17e519a09797fe66e8eaca4632c9e754
HigorMauricio <higormauriciosilva@gmail.com>
2026-04-26 17:41:56 -0300

    feat: criação da landing section da página partnership

--- pages/partnerships.js
+++ pages/partnerships.js
@@ -4,8 +4,12 @@ import styled from 'styled-components';
 import Meta from '../src/infra/Meta';
 
 // images
-
+import bgMobile from '../public/images/partnerships/photos/bg_mobile.png'
+import bgDesktop from '../public/images/partnerships/photos/bg_desktop.png'
 // components
+import Nav from '../src/patterns/base/Nav'; 
+import Button from '../src/components/Button';
+
 
 
 const partnerships = () => {
@@ -18,7 +22,21 @@ const partnerships = () => {
         <>
             {/* Seção incial da pagina onde tem uma frase de impacto e um botão para entrar em contato */}
             <LandingSection>
-                <h1>Pagina em desenvolvimento</h1>
+                <div className='landingSection'>
+                    <div>
+                        <h1>CONECTE SUA MARCA AO FUTURO</h1>
+                    </div>
+                    <div>
+                        <p>
+                            Esteja diante de um <b>público altamente qualificado</b>, fortaleça sua marca no ambiente universitário e compartilhe oportunidades. 
+                            <br></br>
+                            Impacte quem está pronto para transformar o mercado e <b>conquiste hoje os talentos de amanhã</b>
+                        </p>
+                    </div>
+                    <Button>
+                        <b>Quero marcar presença no evento</b>
+                    </Button> 
+                </div>
             </LandingSection>
 
             {/* Seção que explica o porquê ser um parceiro da SSI */}
@@ -52,7 +70,59 @@ const partnerships = () => {
 export default partnerships;
 
 const LandingSection = styled.section`
+    background-image: url(${bgMobile});
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
 
+    min-height: 100vh;
+
+    margin-top: -5rem;
+    padding: 10rem 1rem 5rem 1rem;
+    
+    .landingSection { 
+        display: flex;
+        gap: 2.25rem;
+        flex-direction: column;
+        align-items: center;
+
+
+        h1 {
+            font-size: 2rem; 
+            text-weight: 700;
+            line-height: 2.5rem;
+            text-align: center;
+            
+        }
+
+        p {
+            text-align: center;
+            font-size: 0.875rem;
+            font-weight: 400;
+            line-height: 1.5rem;
+        }
+    }
+
+    @media (min-width: 800px) {
+        background-image: url(${bgDesktop});
+        margin-top: -6rem;
+        padding: 12rem 18.5rem;
+        
+        .ladingSection {
+            gap: 4rem; 
+
+            h1 {
+                font-size: 4rem;
+                line-height: 4.5rem;
+            }
+            
+            p {
+                font-size: 1.125rem;
+                line-height: 1.75rem;
+            }
+        }
+
+    }
 `
 
 const MotivationSection = styled.section`
