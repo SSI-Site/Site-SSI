b84c647bb264f6e7e1142ab99bc609e7577dabb2
fs <fsbarretofilho@gmail.com>
2026-05-04 18:39:52 -0300

    feat: implementação da plans section

--- pages/partnerships.js
+++ pages/partnerships.js
@@ -5,12 +5,21 @@ import Meta from '../src/infra/Meta';
 import Image from 'next/image';
 
 // images
-import bgMobile from '../public/images/partnerships/photos/bg_mobile.png'
-import bgDesktop from '../public/images/partnerships/photos/bg_desktop.png'
+import bgMobile from '../public/images/partnerships/photos/bg_mobile.png';
+import bgDesktop from '../public/images/partnerships/photos/bg_desktop.png';
 import logoInstagram from '../public/images/partnerships/icons/logo_instagram.svg';
 import logoLinkedin from '../public/images/partnerships/icons/logo_linkedin.svg';
 import logoInPerson from '../public/images/partnerships/icons/logo_inPerson.svg';
 import CountUp from 'react-countup';
+
+import logoPartner from '../public/images/partnerships/icons/partner_icon.svg';
+import logoSupporter from '../public/images/partnerships/icons/supporter_icon.svg';
+
+import detailBottonMobile from '../public/images/partnerships/details/detail_botton_mobile.svg';
+import detailUpMobile from '../public/images/partnerships/details/detail_up_mobile.svg';
+
+import detailBottonDesktop from '../public/images/partnerships/details/detail_botton_desktop.svg';
+import detailUpDesktop from '../public/images/partnerships/details/detail_up_desktop.svg';
 
 // components
 import Button from '../src/components/Button';
@@ -59,7 +68,110 @@ const partnerships = () => {
 
             {/* Seção onde tem os planos de patrocínio e o que cada um oferece, com um botões para entrar em contato */}
             <PlansSection>
+                <div className='parceiro'>
+                    <div className='imgMobile'> 
+                        <Image
+                            src={detailUpMobile}
+                            alt='detalhes'
+                            width={196.65}
+                            height={55.74}
+                        />
+                    </div>
+                    <div className='imgDesktop'> 
+                        <Image
+                            src={detailUpDesktop}
+                            alt='detalhes'
+                            width={248}
+                            height={352}
+                        />
+                    </div>
 
+                    <div className='cardParceiro'>
+                        <div className ='title'>
+                            <h4>Parceiro</h4>
+                            <Image
+                                src={logoPartner}
+                                alt='logo'
+                                width={19.21}
+                                height={21.33}
+                                className='logoParceiro'
+                            />
+                        </div>
+                        <div className='info-text'>
+                            <p>
+                                Como parceiro, aproximamos a sua empresa dos alunos e conectamos talentos, 
+                                promovendo a divulgação de processos seletivos, programas de estágio e trainee e 
+                                fortalecemos a imagem da marca perante os universitários. Nossos meios incluem:
+                            </p>
+                            <ul>
+                                <li>Mailing aos alunos;</li>
+                                <li>Posts e vídeos curtos em nossas redes sociais;</li>
+                                <li>Divulgação em panfletos, cartazes, banners e no nosso site;</li>
+                                <li>Atividade da empresa no evento;</li>
+                                <li>Palestras e workshops com temática e conteúdo a critério da empresa;</li>
+                                <li>Fortalecimento de branding via vídeos institucionais, sorteios e brindes.</li>
+                            </ul>
+                            <p>
+                                Construíremos um relacionamento de longo prazo fortalecendo a presença da empresa 
+                                na Universidade e a imagem da marca entre o corpo discente.
+                            </p>
+                        </div>
+                        
+                        <a href="/404">
+                            <Button>
+                                <b>Confira os planos</b>
+                            </Button>
+                        </a>
+                    </div>
+                </div>
+
+                <div className='apoiador'>
+                    <div className='imgMobileApoiador'>
+                        <Image
+                            src={detailBottonMobile}
+                            alt='detalhes'
+                            width={156.39}
+                            height={55.74}
+                        />
+                    </div>
+                    <div className='imgDesktopApoiador'>
+                        <Image
+                            src={detailBottonDesktop}
+                            alt='detalhes'
+                            width={248}
+                            height={248}
+                        />
+                    </div>
+                    <div className='cardApoiador'>
+                        <div className ='title'>
+                            <Image
+                                src={logoSupporter}
+                                alt='logo'
+                                width={19.21}
+                                height={21.33}
+                                className = 'logoApoiador'
+                            />
+                            <h4>Apoiador</h4>
+                        </div>
+                        <div className='info-text'>
+                            <p>
+                                Como apoiador, promovemos sua marca ao nosso público e fornecemos o espaço para atividades da empresa no evento, 
+                                podendo incluir palestras, workshops, sorteios, distribuição de brindes e material promocional, 
+                                entre outros sem qualquer suporte monetário.
+                            </p>
+                           
+                            <p>
+                                Converse conosco e discuta os termos de apoio, fornecemos propostas personalizadas em acordo com o apoiador. 
+                                Garantimos o alcance e fortalecimento da marca.   
+                            </p>
+                        </div>
+                        <a href="/404">
+                            <Button>
+                                <b>Agende uma conversa</b>
+                            </Button>
+                        </a>
+                    </div>
+                </div>
             </PlansSection>
 
             {/* Seção com os numeros da última edição, como impacto nas redes sociais, número de participantes, etc */}
@@ -337,7 +449,187 @@ const SponsorsSection = styled.section`
 `
 
 const PlansSection = styled.section`
-    
+    display: flex;
+    flex-direction: column;
+    gap: 1.5rem;
+    padding: 2.5rem 1rem;
+    align-item: center;
+    align-self: stretch;
+    justify-content: center;
+
+    .parceiro{
+        display: flex;
+        flex-direction: column;
+        gap: 1.5rem;
+
+        .imgDesktop{
+            display: none;
+        }
+
+        .cardParceiro {
+            display: flex;
+            padding: 1rem;
+            flex-direction: column;
+            align-items: flex-start;
+            gap: 0.5rem;
+            align-self: stretch;
+
+            border-radius: 1.5rem;
+            border: 1px solid var(--outline-neutrals-secondary, #999999);
+            background: var(--background-neutrals-primary, #1A1A1A);
+
+            ul {
+                list-style-type: square;
+                padding-left: 1.2rem; 
+            }
+
+            .title {
+                display: flex;
+                flex-direction: row;
+                gap: 0.75rem;
+                align-items: center;
+            }
+
+            .info-text{
+                display: flex;
+                flex-direction: column;
+                gap: 0.5rem;
+
+                p, ul {
+                    color: var(--content-neutrals-primary, #FFF);
+                    font-size: var(--Typograph-Paragraph-Medium-size, 0.875rem);
+                    font-style: normal;
+                    font-weight: 400;
+                    line-height: var(--Typograph-Paragraph-Medium-height, 1.5rem);
+                }
+
+            }
+
+        }    
+    }
+
+    .apoiador {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        gap: 1.5rem;
+
+        .imgDesktopApoiador {
+            display: none;
+        }
+
+        .cardApoiador{
+            display: flex;
+            padding: 1rem;
+            flex-direction: column;
+            align-items: flex-end;
+            gap: 1rem;
+            align-self: stretch;
+
+            border-radius: 1.5rem;
+            border: 1px solid var(--outline-neutrals-secondary, #999);
+            background: var(--background-neutrals-primary, #1A1A1A);
+
+            .title {
+                display: flex;
+                flex-direction: row;
+                align-items: center;
+                gap: 0.75rem;
+            }
+
+            .info-text {
+                display: flex;
+                flex-direction: column;
+                gap: 1rem;
+
+                p {
+                    color: var(--content-neutrals-primary, #FFF);
+                    text-align: right;
+                    font-size: var(--Typograph-Paragraph-Medium-size, 0.875rem);
+                    font-style: normal;
+                    font-weight: 400;
+                    line-height: var(--Typograph-Paragraph-Medium-height, 1.5rem); /* 171.429% */
+                }
+
+            }
+        }
+    }
+
+    @media(min-width: 1100px) {
+        padding: 4.5rem 18.5rem;
+        gap: 1rem;
+
+        .parceiro{
+            flex-direction: row-reverse;
+            justify-content: space-between;
+            padding: 1.5rem;
+
+            .imgMobile {
+                display: none;
+            }
+
+            .imgDesktop{
+                display: block;
+            }
+
+            .title{
+                .logoParceiro{
+                    width: 2.43569rem;
+                    height: 2.75rem;
+                }
+            }
+
+            .cardParceiro{
+                padding: 2.25rem;
+                width: 49.25rem;
+                border-radius: 3rem;
+                gap: 1rem;
+
+                .info-text {
+                    p, ul {
+                        font-size: 1rem;
+                    }
+                }
+            }
+
+
+        }
+
+        .apoiador {
+            flex-direction: row;
+            justify-content: space-between;
+            padding: 1.5rem;
+
+            .cardApoiador{
+                padding: 2.25rem;
+                width: 49.25rem;
+                border-radius: 3rem;
+                gap: 1rem;
+
+                .title {
+                    .logoApoiador{
+                        width: 2.43569rem;
+                        height: 2.75rem;
+                    }
+                }
+
+                .info-text {
+                    p, ul {
+                        font-size: 1rem;
+                    }
+                }
+            }
+
+            .imgDesktopApoiador{
+                display: block;
+            }
+
+            .imgMobileApoiador {
+                display: none;
+            }
+        }
+    }
+
 `
 
 const OurNumbersSection = styled.section`
