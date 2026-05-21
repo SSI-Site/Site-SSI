cd74dff0d334ba1f4ae1a60c5d35914e48520c41
Douglas <Douglas1412@usp.br>
2026-05-21 17:35:50 -0300

    fix: glassmorphism  navbar

--- src/patterns/base/Nav.js
+++ src/patterns/base/Nav.js
@@ -269,7 +269,7 @@ const NavWrapper = styled.div`
     border-radius: 1.5rem; /* era 24px */
     
     /* aqui a gente faz as cores e o efeito glassmorphism da navbar */
-    background: var(--background-neutrals-nav);
+    background: var(--background-neutrals-nav) / 0.75;
     box-shadow: 0 0.125rem 0.25rem 0 rgba(0, 0, 0, 0.25); /* era 2px e 4px para rem */
     backdrop-filter: blur(6px);
 
