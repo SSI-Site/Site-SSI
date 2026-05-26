29049478f40875779acdea5039865181c8a799d0
eduardo <11eduardoalmeida@gmail.com>
2026-05-26 19:41:12 -0300

    add: adiciona variavel edition para eventDetails

--- data/eventDetails.js
+++ data/eventDetails.js
@@ -1,5 +1,6 @@
 export const eventDetails = {
   year: "2026",
+  edition: "17",
   dateText: "24 a 28 de agosto", 
   startDate: "2026-08-24T00:09:30-03:00",
   endDate: "2026-08-28T23:59:59-03:00", 
