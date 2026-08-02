a0e8583bf553fd0e8d7d82e9644b54ce84fdbbf5
Eduardo N Soares <enascimentosoares@usp.br>
2026-08-01 23:50:19 -0300

    logica de expandir e recolher feita

--- src/components/features/schedule/EtecItinerary.js
+++ src/components/features/schedule/EtecItinerary.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import Image from "next/image";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 //images
 import logoEtecDark from "../../../../public/images/schedule/logo-etec-dark.png";
@@ -13,122 +13,126 @@ import dividerMobileDark from "../../../../public/images/schedule/details/divide
 import dividerDesktopDark from "../../../../public/images/schedule/details/divider-desktop-dark.svg";
 
 const EtecItinerary = () => {
+    const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <EtecDetails>
-            <div className="summary">
-                <picture>
-                    <source srcSet={logoEtecLight} media="(prefers-color-scheme: light)" />
-                    <Image
-                        src={logoEtecDark}
-                        alt="logo Etec"
-                        width={200}
-                        height={200}
-                    />
-                </picture>
+        <EtecDetails $isOpen={isOpen}>
+            <div className="etecSchedule">
+                <div className="etecChronogram">
+                    <h4>Cronograma Especial</h4>
 
-                <h4>Teste Sumário</h4>
-            </div>
+                    <picture>
+                        <source srcSet={logoEtecLight} media="(prefers-color-scheme: light)" />
+                        <Image
+                            src={logoEtecDark}
+                            alt="logo Etec"
+                            width={200}
+                            height={200}
+                        />
+                    </picture>
 
-            <div className="event">
-                <h5>O que é a USP?</h5>
-                <h5>10:00 - 10:40h</h5>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="11"
+                        viewBox="0 0 16 11"
+                        fill="none"
+                        onClick={() => setIsOpen((prev) => !prev)}
+                        style={{ cursor: "pointer", transform: isOpen ? "rotate(180deg)" : "rotate(0deg)", transition: "transform 0.2s ease" }}
+                    >
+                        <path d="M2.18125 10.1812L8 4.3624L13.8187 10.1812L16 7.9999L8 -9.82285e-05L0 7.9999L2.18125 10.1812Z" fill="white" />
+                    </svg>
 
-                <p>
-                    Venha saber sobre a maior universidade da América Latina pelos próprios professores e estudantes.
-                </p>
-            </div>
+                </div>
 
-            <picture>
-                <source srcSet={dividerMobileLight} media="(prefers-color-scheme: light)" />
-                <Image
-                    src={dividerMobileDark}
-                    alt="logo Etec"
-                    width={200}
-                    height={200}
-                />
-            </picture>
-
-            <div className="event">
-                <h5>Tour pelo Campus!</h5>
-                <h5>10:50 - 11:40h</h5>
-
-                <p>
-                    Já que só palavras não bastam, também veja com os seus próprios olhos o campus da USP Leste.
-                </p>
-            </div>
-
-            <picture>
-                <source srcSet={dividerMobileLight} media="(prefers-color-scheme: light)" />
-                <Image
-                    src={dividerMobileDark}
-                    alt="logo Etec"
-                    width={200}
-                    height={200}
-                />
-            </picture>
-
-
-            <div className="lunch">
-                <h5>Pausa para o almoço</h5>
-                <h5>10:40 - 12:00h</h5>
-            </div>
-
-            <picture>
-                <source srcSet={dividerMobileLight} media="(prefers-color-scheme: light)" />
-                <Image
-                    src={dividerMobileDark}
-                    alt="logo Etec"
-                    width={200}
-                    height={200}
-                />
-            </picture>
-
-            <div className="event">
-                <h5>Panorama sobre as Àreas de Tecnologia</h5>
-                <h5>12:10 - 13:40h</h5>
-
-                <p>
-                    Quais carreiras posso seguir na área tech? Quais são as diferenças entre os cursos? Venha descobrir!
-                </p>
-            </div>
-
-            <picture>
-                <source srcSet={dividerMobileLight} media="(prefers-color-scheme: light)" />
-                <Image
-                    src={dividerMobileDark}
-                    alt="logo Etec"
-                    width={200}
-                    height={200}
-                />
-            </picture>
-
-            <div className="event">
-                <h5>Oportunidades da USP + Dicas para o Vestibular</h5>
-                <h5>13:50 - 14:30h</h5>
-
-                <p>
-                    Abordaremos as oportunidades que só a USP proporciona, e também dicas para arrasar no vestibular.
-                </p>
-            </div>
-
-            <picture>
-                <source srcSet={dividerMobileLight} media="(prefers-color-scheme: light)" />
-                <Image
-                    src={dividerMobileDark}
-                    alt="logo Etec"
-                    width={200}
-                    height={200}
-                />
-            </picture>
-
-            <div className="event">
-                <h5>Painel das nossas organizações estudantis</h5>
-                <h5>14:40 - 15:30h</h5>
-
-                <p>
-                    Tratemos representantes dos grupos estudantis do curso de Sistemas de informação para esclarecer todas as suas dúvidas!
-                </p>
+                {isOpen && (
+                    <div className="etecDetails">
+                        <div className="etecLecture">
+                            <h5>O que é a USP?</h5>
+                            <h5>10:00 - 10:40h</h5>
+                            <p>
+                                Venha saber sobre a maior universidade da América Latina pelos próprios professores e estudantes.
+                            </p>
+                        </div>
+                        <picture>
+                            <source srcSet={dividerMobileLight} media="(prefers-color-scheme: light)" />
+                            <Image
+                                src={dividerMobileDark}
+                                alt="Linha divisória"
+                                width={200}
+                                height={200}
+                            />
+                        </picture>
+                        <div className="etecLecture">
+                            <h5>Tour pelo Campus!</h5>
+                            <h5>10:50 - 11:40h</h5>
+                            <p>
+                                Já que só palavras não bastam, também veja com os seus próprios olhos o campus da USP Leste.
+                            </p>
+                        </div>
+                        <picture>
+                            <source srcSet={dividerMobileLight} media="(prefers-color-scheme: light)" />
+                            <Image
+                                src={dividerMobileDark}
+                                alt="Linha divisória"
+                                width={200}
+                                height={200}
+                            />
+                        </picture>
+                        <div className="lunchPause">
+                            <h5>Pausa para o almoço</h5>
+                            <h5>10:40 - 12:00h</h5>
+                        </div>
+                        <picture>
+                            <source srcSet={dividerMobileLight} media="(prefers-color-scheme: light)" />
+                            <Image
+                                src={dividerMobileDark}
+                                alt="Linha divisória"
+                                width={200}
+                                height={200}
+                            />
+                        </picture>
+                        <div className="etecLecture">
+                            <h5>Panorama sobre as Àreas de Tecnologia</h5>
+                            <h5>12:10 - 13:40h</h5>
+                            <p>
+                                Quais carreiras posso seguir na área tech? Quais são as diferenças entre os cursos? Venha descobrir!
+                            </p>
+                        </div>
+                        <picture>
+                            <source srcSet={dividerMobileLight} media="(prefers-color-scheme: light)" />
+                            <Image
+                                src={dividerMobileDark}
+                                alt="Linha divisória"
+                                width={200}
+                                height={200}
+                            />
+                        </picture>
+                        <div className="etecLecture">
+                            <h5>Oportunidades da USP + Dicas para o Vestibular</h5>
+                            <h5>13:50 - 14:30h</h5>
+                            <p>
+                                Abordaremos as oportunidades que só a USP proporciona, e também dicas para arrasar no vestibular.
+                            </p>
+                        </div>
+                        <picture>
+                            <source srcSet={dividerMobileLight} media="(prefers-color-scheme: light)" />
+                            <Image
+                                src={dividerMobileDark}
+                                alt="Linha divisória"
+                                width={200}
+                                height={200}
+                            />
+                        </picture>
+                        <div className="etecLecture">
+                            <h5>Painel das nossas organizações estudantis</h5>
+                            <h5>14:40 - 15:30h</h5>
+                            <p>
+                                Tratemos representantes dos grupos estudantis do curso de Sistemas de informação para esclarecer todas as suas dúvidas!
+                            </p>
+                        </div>
+                    </div>
+                )}
             </div>
         </EtecDetails>
     )
@@ -137,5 +141,7 @@ const EtecItinerary = () => {
 export default EtecItinerary;
 
 const EtecDetails = styled.article`
-
+    .etecDetails {
+        display: ${({ $isOpen }) => ($isOpen ? "block" : "none")};
+    }
 `
