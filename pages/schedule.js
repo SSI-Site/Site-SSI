6a5fe71e84992f234f32d4ce29a0830a0879c856
Guilherme Aika Alves Varella <guilherme.avarella@gmail.com>
2026-08-03 01:49:04 -0300

    TEMPORÁRIO: algumas seções temporárias enquanto não temos todos os dados no sistema

--- pages/schedule.js
+++ pages/schedule.js
@@ -12,6 +12,9 @@ import DateStamp from '../src/components/features/schedule/DateStamp';
 import ScheduleItems from '../src/components/features/schedule/ScheduleItems';
 import saphira from '../services/saphira';
 
+// TEMPORÁRIO
+import { LinkedInLogo, InstagramLogo, YouTubeLogo } from '../src/components/ui/SocialMediaLogos';
+
 import { eventDetails } from '../data/eventDetails';
 
 const Schedule = () => {
@@ -82,6 +85,46 @@ const Schedule = () => {
         setDayNumber(0); 
     }
 }, [talks, activeItem, dayFull]);
+
+    {/* 
+        =====================================================
+        CONTEÚDO TEMPORÁRIO. BASTA DELETAR ESSE RETURN ABAIXO
+        ===================================================== 
+    */}
+    return (
+        <section style={{padding: '15rem 0', textAlign: 'center', gap: '2rem', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+            <h3>Em breve você poderá conferir a programação</h3>
+            <p>Acompanhe nossas redes sociais para receber as atualizações em primeira mão.<br/>Não perca nenhuma novidade!</p>
+            <div style={{display: 'flex', gap: '1rem', transform: 'scale(1.5)', alignItems: 'center', justifyContent: 'center', marginTop: '1rem'}}>
+                <a
+                    href="https://www.instagram.com/semanadesi/"
+                    target="_blank"
+                    aria-label="Instagram da Semana de Sistemas de Informação"
+                >
+                    {/*Instagram Logo*/}
+                    <InstagramLogo />
+                </a>
+
+                <a
+                    href="https://www.linkedin.com/company/comissão-organizadora-da-semana-de-sistemas-de-informação"
+                    target="_blank"
+                    aria-label="Linkedin da Semana de Sistemas de Informação"
+                >
+                    {/*Linkedin Logo*/}
+                    <LinkedInLogo />
+                </a>
+
+                <a
+                    href="https://www.youtube.com/@semanadesi"
+                    target="_blank"
+                    aria-label="YouTube da Semana de Sistemas de Informação"
+                >
+                    {/* YouTube Logo */}
+                    <YouTubeLogo />
+                </a>
+            </div>
+        </section>
+    )
 
     return (
         <>
