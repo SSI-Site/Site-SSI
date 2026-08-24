a7fff70f68687da92bbd34bd5c5cf849257a0a3c
Guilherme Aika Alves Varella <guilherme.avarella@gmail.com>
2026-08-24 17:43:44 -0300

    fix: user progress always null

--- pages/user.js
+++ pages/user.js
@@ -107,10 +107,6 @@ const User = () => {
     })
 
     const getStudentInfo = async() => {
-        if (!user) return;
-
-        setIsLoading(true);
-
         try{
             const { data } = await saphira.getStudent()
             if (data) setStudentInfo({ ...saphiraUserDataToFormFormat(data) });
@@ -181,16 +177,25 @@ const User = () => {
     }
 
     useEffect(() => {
-        if (loading) return; 
-
         if (disableAuth || !user) {
             Router.push('/');
-        } else {
-            getStudentInfo();
-            getPresences();
-            getStudentGifts();
-        }
-    }, [user, loading]);
+            return;
+        } 
+        
+        const fetchAllData = async () => {
+            setIsLoading(true);
+
+            await Promise.all([
+                getStudentInfo(),
+                getPresences(),
+                getStudentGifts()
+            ]);
+            
+            setIsLoading(false);
+        };
+
+        fetchAllData();
+    }, [user, disableAuth]);
 
     const handleShowCodeModal = () => {
         setIsOpen(false);
